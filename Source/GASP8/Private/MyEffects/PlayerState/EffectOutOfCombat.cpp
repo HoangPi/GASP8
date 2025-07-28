3086e8777231f571f6129630974a3777d0caecfb
LAV <voduchoangphi@gmail.com>
2025-07-28 12:42:29 +0700

    out of combat effect

---
+++ Source/GASP8/Private/MyEffects/PlayerState/EffectOutOfCombat.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyEffects/PlayerState/EffectOutOfCombat.h"
+
+#include "MyEffects/Attribute/Stamina/EffectRestoreStamina.h"
+
+UEffectOutOfCombat::UEffectOutOfCombat()
+{
+    this->StackingType = EGameplayEffectStackingType::AggregateByTarget;
+    this->StackLimitCount = 1;
+    this->StackDurationRefreshPolicy = EGameplayEffectStackingDurationPolicy::RefreshOnSuccessfulApplication;
+
+    this->DurationPolicy = EGameplayEffectDurationType::HasDuration;
+    this->DurationMagnitude = FGameplayEffectModifierMagnitude(2.0f);
+
+    UAdditionalEffectsGameplayEffectComponent *comp = this->CreateDefaultSubobject<UAdditionalEffectsGameplayEffectComponent>(FName("uhmmmm"));
+    comp->OnCompleteNormal.Add(UEffectRestoreStamina::StaticClass());
+}
---
+++ Source/GASP8/Private/MyEffects/PlayerState/EffectOutOfCombat.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+
+#include "GameplayEffectComponents/AdditionalEffectsGameplayEffectComponent.h"
+
+#include "EffectOutOfCombat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEffectOutOfCombat : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+public:
+	UEffectOutOfCombat();
+};
