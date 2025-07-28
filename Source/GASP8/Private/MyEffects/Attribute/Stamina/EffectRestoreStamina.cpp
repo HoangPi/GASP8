af0f4517d492776a1c67095d4a37adc0fea91a2a
LAV <voduchoangphi@gmail.com>
2025-07-28 14:40:58 +0700

    no, set by caller (no caller)

--- Source/GASP8/Private/MyEffects/Attribute/Stamina/EffectRestoreStamina.cpp
+++ Source/GASP8/Private/MyEffects/Attribute/Stamina/EffectRestoreStamina.cpp
@@ -11,10 +11,8 @@ UEffectRestoreStamina::UEffectRestoreStamina()
     this->DurationPolicy = EGameplayEffectDurationType::Infinite;
     this->Period = 0.012f;
     FGameplayModifierInfo mod;
-    FSetByCallerFloat callerMag;
-    callerMag.DataTag = Tags::Attribute::stamina;
     mod.Attribute = UAttributeStamina::GetStaminaAttribute();
-    mod.ModifierMagnitude = FGameplayEffectModifierMagnitude(callerMag);
+    mod.ModifierMagnitude = FGameplayEffectModifierMagnitude(1.1f);
     mod.ModifierOp = EGameplayModOp::Additive;
     this->Modifiers.Add(mod);
     this->CachedAssetTags.AddTag(Tags::EffectType::idle);
