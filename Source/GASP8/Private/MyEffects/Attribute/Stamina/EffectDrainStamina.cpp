1d3f51d1d69f2a9222993ea355edbeb97d0cad24
LAV <voduchoangphi@gmail.com>
2025-07-28 14:11:35 +0700

    add drain stamina effect

---
+++ Source/GASP8/Private/MyEffects/Attribute/Stamina/EffectDrainStamina.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyEffects/Attribute/Stamina/EffectDrainStamina.h"
+
+#include "MyAttributes/Stamina/AttributeStamina.h"
+#include "MyTags/MyTags.h"
+
+UEffectDrainStamina::UEffectDrainStamina()
+{
+    this->DurationPolicy = EGameplayEffectDurationType::Infinite;
+    FGameplayModifierInfo mod;
+    FSetByCallerFloat callerMag;
+    callerMag.DataTag = Tags::Attribute::stamina;
+    mod.Attribute = UAttributeStamina::GetStaminaAttribute();
+    mod.ModifierMagnitude = FGameplayEffectModifierMagnitude(callerMag);
+    mod.ModifierOp = EGameplayModOp::Additive;
+    this->Modifiers.Add(mod);
+}
