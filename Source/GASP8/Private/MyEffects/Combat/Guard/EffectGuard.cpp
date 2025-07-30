a78b9e9183b91289bd754dcadff2cb7b4dcb428c
LAV <voduchoangphi@gmail.com>
2025-07-30 12:13:18 +0700

    guard effect

---
+++ Source/GASP8/Private/MyEffects/Combat/Guard/EffectGuard.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyEffects/Combat/Guard/EffectGuard.h"
+
+#include "Ultilities/GobalVars.h"
+
+UEffectGuard::UEffectGuard()
+{
+    this->StackingType = EGameplayEffectStackingType::AggregateByTarget;
+    this->StackLimitCount = 1;
+    this->StackDurationRefreshPolicy = EGameplayEffectStackingDurationPolicy::RefreshOnSuccessfulApplication;
+    this->StackExpirationPolicy = EGameplayEffectStackingExpirationPolicy::ClearEntireStack;
+
+    this->DurationPolicy = EGameplayEffectDurationType::HasDuration;
+    this->DurationMagnitude = FGameplayEffectModifierMagnitude(GuardDuration);
+}
