e6b77d3afe93b52a8ad925193bac67ef918245df
LAV <voduchoangphi@gmail.com>
2025-07-30 12:29:08 +0700

    pragma once

--- Source/GASP8/Private/Ultilities/GobalVars.h
+++ Source/GASP8/Private/Ultilities/GobalVars.h
@@ -1,2 +1,4 @@
+#pragma once
+
 volatile float DeflectDuration = 2.0f;
 volatile float GuardDuration = 2.0f;
--- Source/GASP8/Private/Ultilities/Macro.h
+++ Source/GASP8/Private/Ultilities/Macro.h
@@ -1,1 +1,3 @@
+#pragma once
+
 #define CLAMP(value, min, max) (value > max ? max : (value < min ? min : value)) 
