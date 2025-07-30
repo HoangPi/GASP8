198b3c116b353004f7577081e091e71409f265f2
LAV <voduchoangphi@gmail.com>
2025-07-30 13:06:58 +0700

    add tag

--- Source/GASP8/Private/MyTags/PlayerState/PlayerState.cpp
+++ Source/GASP8/Private/MyTags/PlayerState/PlayerState.cpp
@@ -5,5 +5,6 @@ namespace Tags
     namespace PlayerState
     {
         UE_DEFINE_GAMEPLAY_TAG(combat, "player_state.combat")
+        UE_DEFINE_GAMEPLAY_TAG(manual_guard, "player_state.manual_guard")
     }
 }
