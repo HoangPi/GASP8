4b9f9e05eae8bc9016033a5c79a349f12a17d4a0
LAV <voduchoangphi@gmail.com>
2025-07-30 13:06:18 +0700

    idk, it uses to work

--- Source/GASP8/Private/Ultilities/GobalVars.h
+++ Source/GASP8/Private/Ultilities/GobalVars.h
@@ -1,4 +1,7 @@
-#pragma once
+// #ifndef THOSE_GLOBAL_VARIABLES
+// #define THOSE_GLOBAL_VARIABLES
 
-volatile float DeflectDuration = 2.0f;
-volatile float GuardDuration = 2.0f;
+// volatile float DeflectDuration = 2.0f;
+// volatile float GuardDuration = 2.0f;
+
+// #endif
