ac1f0d2663aa476e16474cdbce06e0b3b3e840ad
LAV <voduchoangphi@gmail.com>
2025-07-30 11:40:46 +0700

    disrupt idle state when activate guard

---
+++ Source/GASP8/Private/AbilityGuard.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilityGuard.h"
+
+#include "MyEffects/PlayerState/EffectOutOfCombat.h"
+#include "MyTags/MyTags.h"
+
+UAbilityGuard::UAbilityGuard()
+{
+    this->ShouldEnterCombat = false;
+}
+
+void UAbilityGuard::ActivateAbility(
+    FGameplayAbilitySpecHandle Handle, 
+    const FGameplayAbilityActorInfo *ActorInfo, 
+    FGameplayAbilityActivationInfo ActivationInfo, 
+    const FGameplayEventData *TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+    UAbilitySystemComponent *ownerASC = ActorInfo->AbilitySystemComponent.Get();
+    ownerASC->RemoveActiveEffectsWithTags(UAbilityBase::IdleTags);
+    ownerASC->ApplyGameplayEffectToSelf(
+        (UEffectOutOfCombat *)UEffectOutOfCombat::StaticClass()->GetDefaultObject(),
+        1.0f,
+        ownerASC->MakeEffectContext()
+    );
+}
