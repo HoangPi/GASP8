2dfe6ceeabfd36d8debc1a23ad226764fdd76636
LAV <voduchoangphi@gmail.com>
2025-08-01 21:06:17 +0700

    health attribute

---
+++ Source/GASP8/Private/MyAttributes/Health/AttributeHealth.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAttributes/Health/AttributeHealth.h"
+
+#include "Ultilities/Macro.h"
+
+UAttributeHealth::UAttributeHealth()
+{
+    UDataTable *table = LoadObject<UDataTable>(nullptr, TEXT("/Game/ThirdPerson/DataTable/DT_Health.DT_Health"));
+    FAttributeMetaData *data = table->FindRow<FAttributeMetaData>(FName("3"), FString("NoThanks"));
+    this->Health = data->BaseValue;
+    this->MaxHealth = data->BaseValue;
+}
+
+void UAttributeHealth::PreAttributeChange(const FGameplayAttribute &Attribute, float &NewValue)
+{
+    Super::PreAttributeChange(Attribute, NewValue);
+    float max;
+    if(Attribute == UAttributeHealth::GetHealthAttribute())
+    {
+        max = this->MaxHealth.GetCurrentValue();
+        NewValue = CLAMP(NewValue, 0.0f, max);
+        this->OnHealthChange.Broadcast(NewValue/max);
+    }
+}
