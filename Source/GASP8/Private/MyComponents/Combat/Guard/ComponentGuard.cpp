ec525c2cd2e4f79f5bf703cfa9664b2f684a5075
LAV <voduchoangphi@gmail.com>
2025-08-01 13:48:30 +0700

    give ability

--- Source/GASP8/Private/MyComponents/Combat/Guard/ComponentGuard.cpp
+++ Source/GASP8/Private/MyComponents/Combat/Guard/ComponentGuard.cpp
@@ -4,6 +4,7 @@
 
 #include "GASP8/GASP8Character.h"
 #include "MyAbilities/Combat/Guard/AbilityGuard.h"
+#include "MyAbilities/Combat/Guard/AbilityGetHit.h"
 
 // Sets default values for this component's properties
 UComponentGuard::UComponentGuard()
@@ -17,6 +18,7 @@ UComponentGuard::UComponentGuard()
 	{
 		this->OwnerASC = owner->GetAbilitySystemComponent();
 		this->GuardHandle = this->OwnerASC->K2_GiveAbility(UAbilityGuard::StaticClass());
+		this->OwnerASC->K2_GiveAbility(UAbilityGetHit::StaticClass());
 	}
 	this->GuardAction = LoadObject<UInputAction>(nullptr, TEXT("/Game/ThirdPerson/Input/Actions/IA_Guard.IA_Guard"));
 }
