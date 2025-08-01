7eec50aa2dcceb9e7faef6d63a0b6a315c658731
LAV <voduchoangphi@gmail.com>
2025-08-01 14:13:25 +0700

    associate itself with guard ability and handle events

--- Source/GASP8/Private/MyAbilities/Combat/Guard/AbilityGetHit.cpp
+++ Source/GASP8/Private/MyAbilities/Combat/Guard/AbilityGetHit.cpp
@@ -30,11 +30,29 @@ void UAbilityGetHit::ActivateAbility(
     }
     Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
     UAbilitySystemComponent *ownerASC = ActorInfo->AbilitySystemComponent.Get();
-    GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, FString("I GOT HIT"));
+    if (ownerASC->GetActiveGameplayEffect(*this->DeflectHandle))
+    {
+        ownerASC->RemoveActiveGameplayEffect(*this->DeflectHandle, 1);
+    }
+    else if (ownerASC->GetActiveGameplayEffect(*this->GuardHandle))
+    {
+        ownerASC->RemoveActiveGameplayEffect(*this->GuardHandle, 1);
+    }
+    else if (ownerASC->HasMatchingGameplayTag(Tags::PlayerState::manual_guard))
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString("Dull block"));
+    }
+    else
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString("Full hit"));
+    }
     this->EndAbility(Handle, ActorInfo, ActivationInfo, false, false);
 }
 
 void UAbilityGetHit::OnAvatarSet(const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilitySpec &Spec)
 {
     Super::OnAvatarSet(ActorInfo, Spec);
+    UAbilityGuard *guard = (UAbilityGuard *)ActorInfo->AbilitySystemComponent.Get()->FindAbilitySpecFromClass(UAbilityGuard::StaticClass())->GetPrimaryInstance();
+    this->DeflectHandle = &guard->DeflectHandle;
+    this->GuardHandle = &guard->GuardHandle;
 }
