9af06ba08312fc885550b062401e827cbbb772b7
LAV <voduchoangphi@gmail.com>
2025-08-06 11:01:36 +0700

    end chase sequence task

---
+++ Source/GASP8/Private/MyAI/Task/BTT_EndChaseSequence.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAI/Task/BTT_EndChaseSequence.h"
+
+#include "MyAI/Controller/AIC_PatrolNative.h"
+
+UBTT_EndChaseSequence::UBTT_EndChaseSequence()
+{
+}
+
+EBTNodeResult::Type UBTT_EndChaseSequence::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory)
+{
+    EBTNodeResult::Type earlyCheck = Super::ExecuteTask(OwnerComp, NodeMemory);
+    if(earlyCheck != EBTNodeResult::Succeeded)
+    {
+        return earlyCheck;
+    }
+    if(ACharacter *owner = Cast<ACharacter>(OwnerComp.GetAIOwner()->GetPawn()))
+    {
+        owner->GetCharacterMovement()->MaxWalkSpeed = 120.0f;
+    }
+    if(AAIC_PatrolNative *controller = Cast<AAIC_PatrolNative>(OwnerComp.GetAIOwner()))
+    {
+        controller->RunBehaviorTree(controller->PatrolTree);
+        return EBTNodeResult::Succeeded;
+    }
+    return EBTNodeResult::Failed;
+}
